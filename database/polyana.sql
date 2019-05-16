9856531e3f5a5541a2235026e0b6e120e7e632a4
Andrei Dziaineka <dziaineka@gmail.com>
2019-05-16 20:27:53 +0300

    database scheme sql

---
+++ database/polyana.sql
@@ -0,0 +1,92 @@
+CREATE TABLE "player" (
+  "id" int PRIMARY KEY,
+  "nickname" text NOT NULL,
+  "password" text NOT NULL,
+  "token" text,
+  "token_expiration" timestamp without time zone,
+  "played_battles" int NOT NULL,
+  "battles_won" int NOT NULL,
+  "winrate" float,
+
+  CONSTRAINT player_nickname_key UNIQUE (nickname),
+  CONSTRAINT player_token_key UNIQUE (token)
+);
+
+CREATE TYPE event_type AS ENUM('registration',
+                               'battle_start',
+                               'battle_end',
+                               'money_buying',
+                               'achievement',
+                               'login',
+                               'logout');
+
+CREATE TABLE "event" (
+  "id" int PRIMARY KEY,
+  "player_id" int NOT NULL,
+  "type" event_type NOT NULL,
+  "source" int,
+  "payload" jsonb,
+  "created" timestamp without time zone DEFAULT timezone('UTC'::text, now())
+);
+
+CREATE TABLE "transaction" (
+  "id" int PRIMARY KEY,
+  "event_id" int NOT NULL,
+  "player_id" int NOT NULL,
+  "currency_id" int NOT NULL,
+  "amount" int
+);
+
+CREATE TYPE currency_type AS ENUM('silver',
+                                  'gold');
+
+CREATE TABLE "currency" (
+  "id" int PRIMARY KEY,
+  "type" currency_type NOT NULL,
+  "rate" float NOT NULL
+);
+
+CREATE TYPE achievement_type AS ENUM('5_battles',
+                                     '5_wins',
+                                     'first_win');
+
+CREATE TABLE "achievement" (
+  "id" int PRIMARY KEY,
+  "type" achievement_type NOT NULL,
+  "player_id" int NOT NULL,
+  "created" timestamp without time zone DEFAULT timezone('UTC'::text, now())
+);
+
+CREATE TABLE "money" (
+  "id" int PRIMARY KEY,
+  "player_id" int NOT NULL,
+  "currency_id" int NOT NULL,
+  "amount" int
+);
+
+CREATE TABLE "battle" (
+  "id" int PRIMARY KEY,
+  "currency_id" int NOT NULL,
+  "bid" int,
+  "winner" int NOT NULL,
+  "participants" int[] NOT NULL,
+  "created" timestamp without time zone DEFAULT timezone('UTC'::text, now())
+);
+
+ALTER TABLE "event" ADD FOREIGN KEY ("player_id") REFERENCES "player" ("id");
+
+ALTER TABLE "transaction" ADD FOREIGN KEY ("event") REFERENCES "event" ("id");
+
+ALTER TABLE "transaction" ADD FOREIGN KEY ("player_id") REFERENCES "player" ("id");
+
+ALTER TABLE "transaction" ADD FOREIGN KEY ("currency_id") REFERENCES "currency" ("id");
+
+ALTER TABLE "achievement" ADD FOREIGN KEY ("player_id") REFERENCES "player" ("id");
+
+ALTER TABLE "money" ADD FOREIGN KEY ("player_id") REFERENCES "player" ("id");
+
+ALTER TABLE "money" ADD FOREIGN KEY ("currency_id") REFERENCES "currency" ("id");
+
+ALTER TABLE "battle" ADD FOREIGN KEY ("currency_id") REFERENCES "currency" ("id");
+
+ALTER TABLE "battle" ADD FOREIGN KEY ("winner") REFERENCES "player" ("id");
