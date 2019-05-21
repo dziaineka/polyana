0df8a2522f3c4e9a006048a4c73322a86fc43db6
Andrei Dziaineka <dziaineka@gmail.com>
2019-05-21 20:19:01 +0300

    matchmaker works properly without room creation

--- battle_server/polyana/apps/polyana/src/matchmaker/pl_matchmaker_wrk.erl
+++ battle_server/polyana/apps/polyana/src/matchmaker/pl_matchmaker_wrk.erl
@@ -3,18 +3,84 @@
 -export([start_link/0]).
 -export([init/0]).
 
+-record(state, {
+    players_amount = 2,
+    rating_delta = 0.01,
+    expand_multiplier = 2
+}).
+
 start_link() ->
     Pid = spawn_link(?MODULE, init, []),
     {ok, Pid}.
 
 init() ->
-    % probably have to initialize some state, but it is not clear so far
+    lager:info("Starting matching process."),
+    State = #state{},
+    loop(State).
 
-    loop().
+loop(#state{players_amount = Amount,
+            rating_delta = Delta,
+            expand_multiplier = Multiplier} = State) ->
+    timer:sleep(2000),
 
-loop() ->
-    % matching
-    lager:info("matching process"),
-    timer:sleep(30000),
+    case ets:first(pl_queue_srv) of
+        '$end_of_table' ->
+            loop(State);
 
-    loop().
+        _ ->
+            Players = match_players(Amount, Delta),
+
+            if
+                Amount == length(Players) ->
+                    remove_from_queue(Players),
+                    create_room(Players),
+                    loop(State);
+
+                true ->
+                    NewDelta = Delta * Multiplier,
+
+                    loop(#state{players_amount = Amount,
+                                rating_delta = NewDelta,
+                                expand_multiplier = Multiplier})
+            end
+    end.
+
+match_players(Amount, Delta) ->
+    PlayersAndRatings = ets:foldl(
+        fun ({PlayerPid, _PlayerId, Rating}, Players) ->
+            case length(Players) of
+                Amount ->
+                    Players;
+
+                0 ->
+                    [{PlayerPid, Rating}];
+
+                _ ->
+                    [{_Pid, EtalonRating} | _] = Players,
+
+                    if
+                        abs(EtalonRating - Rating) =< Delta ->
+                            lists:append(Players, [{PlayerPid, Rating}]);
+
+                        true ->
+                            Players
+                    end
+            end
+        end,
+        [],
+        pl_queue_srv
+    ),
+
+    {Players, _Ratings} = lists:unzip(PlayersAndRatings),
+    Players.
+
+remove_from_queue(Players) ->
+    lists:foreach(
+        fun (PlayerPid) ->
+            pl_queue_srv:delete_player(PlayerPid)
+        end,
+        Players
+    ).
+
+create_room(Players) ->
+    lager:info("create room ~p", [Players]).
