c1fa4363e6881feaa9712057a6bfe922ecb39e68
dziaineka <dziaineka@gmail.com>
2019-05-26 10:46:16 +0300

    test code deleted

--- battle_server/polyana/apps/polyana/src/matchmaker/pl_queue_srv.erl
+++ battle_server/polyana/apps/polyana/src/matchmaker/pl_queue_srv.erl
@@ -6,10 +6,9 @@
 -export([stop/1, start_link/0, add_player/3, delete_player/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
--export([find_game/1]). %убрать тест
--record(state, {dummy
-            , match % убрать тест
-}).
+
+-record(state, {dummy}).
+
 stop(Name) ->
     gen_server:call(Name, stop).
 
@@ -20,31 +19,13 @@ add_player(PlayerPid, PlayerId, {Currency, Bid}) ->
 delete_player(PlayerPid) ->
     gen_server:cast(?MODULE, {delete_player, PlayerPid}).
 
-%% убрать, начало для теста
-
-find_game(PlayerSrv) ->
-    gen_server:call(?MODULE, {find_game, PlayerSrv}).
-%% конец для теста
-
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init(_Args) ->
     lager:info("queue manager is starting"),
     ets:new(?MODULE, [named_table]),
-    {ok, #state{dummy=1
-            , match = #{} % убрать
-    }}.
-
-%% убрать, код из handle_call для теста
-
-handle_call({find_game, PlayerSrv}, _From, #state{match = Match}=State) ->
-    {Reply, Match2} =  in_find_game(PlayerSrv, Match),
-    {Reply1, Match3}= check_condition(Reply, Match2),
-    State2 = State#state{match = Match3},
-    {reply, Reply1, State2};
-
-%% убрать
+    {ok, #state{dummy=1}}.
 
 handle_call(stop, _From, State) ->
     {stop, normal, stopped, State};
@@ -81,23 +62,3 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
-
-
-
-%%убрать, для теста
-
-in_find_game(PlayerSrv, Match) ->
-    case maps:find(PlayerSrv, Match) of
-        {ok, _} ->
-             {<<"Please, Waiting\n">>, Match};
-        error -> Match2 = maps:put(PlayerSrv, none, Match),
-            {<<"Searching Game\n">>, Match2}
-    end.
-
-check_condition(_Reply, Match) when map_size(Match) == 3 ->
-    pl_matchmaker_wrk:create_room(Match);
-check_condition(Reply, Match) ->
-    {{single, Reply}, Match}.
-
-
-%% убрать, для теста
