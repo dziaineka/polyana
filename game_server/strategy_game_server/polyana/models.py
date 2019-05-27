beab01f7311b96a36a783f32c3d57c989d0d47d4
Andrei Dziaineka <dziaineka@gmail.com>
2019-05-27 23:28:25 +0300

    user_id in polyana_player can be null

--- game_server/strategy_game_server/polyana/models.py
+++ game_server/strategy_game_server/polyana/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Player(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     nickname = models.CharField(max_length=200, null=False, blank=False, unique=True)
     password = models.CharField(max_length=200, null=False, blank=False)
     token = models.CharField(max_length=200, null=True, blank=True, unique=True)
