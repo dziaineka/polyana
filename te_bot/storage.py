532984bd77e927c90fcfb4ab15a4861e98f88c67
Anton <gtrsaeryg@gmail.com>
2019-05-30 04:09:42 +0300

    Interface for the  game "polyana" for the web application "telegram" author: "Anton Paduta" version: 1.0 Beta

---
+++ te_bot/storage.py
@@ -0,0 +1,13 @@
+import telnetlib
+import time
+
+
+class Storage:
+    def __init__(self):
+        self.connect= {} #dict
+    def add_connect(self,user_id):
+        tn = telnetlib.Telnet("localhost",1234)
+        time.sleep(0.1)
+        self.connect[user_id] = tn
+    def get_connect(self,user_id):
+        return self.connect[user_id]
