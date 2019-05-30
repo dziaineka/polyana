f797d7cbde7e63e6452ea0cd7fff6cfa7948e8d7
dziaineka <dziaineka@gmail.com>
2019-05-30 10:58:13 +0300

    pepization

--- te_bot/storage.py
+++ te_bot/storage.py
@@ -4,10 +4,12 @@ import time
 
 class Storage:
     def __init__(self):
-        self.connect= {} #dict
-    def add_connect(self,user_id):
-        tn = telnetlib.Telnet("localhost",1234)
+        self.connect = {}
+
+    def add_connect(self, user_id):
+        tn = telnetlib.Telnet("localhost", 1234)
         time.sleep(0.1)
         self.connect[user_id] = tn
-    def get_connect(self,user_id):
+
+    def get_connect(self, user_id):
         return self.connect[user_id]
